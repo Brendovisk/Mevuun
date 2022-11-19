cb278a7231ddc25d4a1f7a4dd2d499f084647c71
Brendon <brendon.franco@vibryt.com.br>
2022-11-19 15:19:47 -0300

    feat: add responsive_grid package to wrap the game cards

--- lib/all_games_page.dart
+++ lib/all_games_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meevun_2/search_all_games_page.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 import 'add_new_game_page.dart';
 
 class AllGamesPage extends StatefulWidget {
@@ -47,82 +48,92 @@ class AllGamesPageState extends State<AllGamesPage> {
           )
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Theme.of(context).colorScheme.onBackground,
-        child: Icon(
-          Icons.add,
-          color: Theme.of(context).colorScheme.onPrimary,
+      floatingActionButton: SizedBox(
+        width: 75,
+        height: 75,
+        child: FloatingActionButton(
+          backgroundColor: Theme.of(context).colorScheme.onBackground,
+          child: Icon(
+            Icons.add,
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (_) => const AddNewgame(),
+              ),
+            );
+          },
         ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (_) => const AddNewgame(),
-            ),
-          );
-        },
       ),
       body: SafeArea(
         top: false,
         bottom: false,
-        child: (Padding(
-          padding: const EdgeInsets.all(25),
-          child: ListView(
-            children: [
-              Card(
-                elevation: 5,
-                margin: const EdgeInsets.only(bottom: 20),
-                color: Theme.of(context).colorScheme.surfaceVariant,
-                child: InkWell(
-                  splashColor: Theme.of(context).colorScheme.surfaceVariant,
-                  onTap: () {
-                    debugPrint("Card Clicked");
-                  },
-                  child: SizedBox(
-                    height: 300,
-                    child: Stack(
-                      children: [
-                        ClipRRect(
-                          borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(14),
-                            topRight: Radius.circular(14),
-                          ),
-                          child: Image.network(
-                            "https://media.rawg.io/media/crop/600/400/games/1c3/1c305096502c475c00276c827f0fd697.jpg",
-                            height: 200,
-                            width: double.infinity,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                        Positioned(
-                          bottom: 30,
-                          left: 15,
-                          child: Text(
-                            "God of War".toUpperCase(),
-                            style: const TextStyle(
-                              fontSize: 28,
-                              fontWeight: FontWeight.w600,
+        child: ListView(
+          children: [
+            ResponsiveGridRow(
+              children: [
+                ResponsiveGridCol(
+                  lg: 3,
+                  md: 6,
+                  xs: 12,
+                  child: Card(
+                    elevation: 5,
+                    margin: const EdgeInsets.fromLTRB(15, 0, 15, 30),
+                    color: Theme.of(context).colorScheme.surfaceVariant,
+                    child: InkWell(
+                      splashColor: Theme.of(context).colorScheme.surfaceVariant,
+                      onTap: () {
+                        debugPrint("Card Clicked");
+                      },
+                      child: SizedBox(
+                        height: 300,
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(14),
+                                topRight: Radius.circular(14),
+                              ),
+                              child: Image.network(
+                                "https://media.rawg.io/media/crop/600/400/games/1c3/1c305096502c475c00276c827f0fd697.jpg",
+                                height: 200,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
                             ),
-                          ),
+                            Positioned(
+                              bottom: 30,
+                              left: 15,
+                              child: Text(
+                                "God of War".toUpperCase(),
+                                style: const TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ),
+                            Positioned(
+                              right: 10,
+                              bottom: 110,
+                              child: FloatingActionButton(
+                                child: const Icon(Icons.star_outline),
+                                onPressed: () {
+                                  debugPrint("Make favorite");
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                        Positioned(
-                          right: 20,
-                          bottom: 120,
-                          child: FloatingActionButton(
-                            child: const Icon(Icons.star_outline),
-                            onPressed: () {
-                              debugPrint("Make favorite");
-                            },
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        )),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
