3224ff6846db8d3cad743fd50660e8f949c1a895
Brendon <brendon.franco@vibryt.com.br>
2022-11-19 13:17:59 -0300

    feat: add the game cards widget to the home screen

--- lib/all_games_page.dart
+++ lib/all_games_page.dart
@@ -59,6 +59,143 @@ class AllGamesPageState extends State<AllGamesPage> {
           );
         },
       ),
+      body: SafeArea(
+        top: false,
+        bottom: false,
+        child: (Padding(
+          padding: const EdgeInsets.all(25),
+          child: ListView(
+            children: [
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: InkWell(
+                  splashColor: Theme.of(context).colorScheme.surfaceVariant,
+                  onTap: () {
+                    debugPrint("Card Clicked");
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(14),
+                            topRight: Radius.circular(14),
+                          ),
+                          child: Image.network(
+                            'https://media.rawg.io/media/resize/1920/-/screenshots/dc5/dc5d251100154f7502b4b8510591cc5d.jpg',
+                            height: 200,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 20, 15, 10),
+                          child: Text(
+                            "Cuphead",
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: InkWell(
+                  splashColor: Theme.of(context).colorScheme.surfaceVariant,
+                  onTap: () {
+                    debugPrint("Card Clicked");
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(14),
+                            topRight: Radius.circular(14),
+                          ),
+                          child: Image.network(
+                            'https://media.rawg.io/media/resize/1920/-/screenshots/dc5/dc5d251100154f7502b4b8510591cc5d.jpg',
+                            height: 200,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 20, 15, 10),
+                          child: Text(
+                            "Cuphead",
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                elevation: 5,
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                color: Theme.of(context).colorScheme.surfaceVariant,
+                child: InkWell(
+                  splashColor: Theme.of(context).colorScheme.surfaceVariant,
+                  onTap: () {
+                    debugPrint("Card Clicked");
+                  },
+                  child: SizedBox(
+                    width: double.infinity,
+                    height: 300,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        ClipRRect(
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(14),
+                            topRight: Radius.circular(14),
+                          ),
+                          child: Image.network(
+                            'https://media.rawg.io/media/resize/1920/-/screenshots/dc5/dc5d251100154f7502b4b8510591cc5d.jpg',
+                            height: 200,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.fromLTRB(15, 20, 15, 10),
+                          child: Text(
+                            "Cuphead",
+                            style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
