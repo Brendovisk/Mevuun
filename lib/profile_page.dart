ba3656565396a349ced5cf4c213ec9dbf6577d3d
Brendon <brendon.franco@vibryt.com.br>
2022-11-20 01:39:56 -0300

    feat: add the profile edit page

--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:meevun_2/profile_edit_page.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -14,6 +15,25 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 10, 10),
+            child: IconButton(
+              icon: const Icon(
+                Icons.edit,
+                size: 28,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) => const ProfileEditPage(),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
         title: const Text(
           "Profile",
           style: TextStyle(
@@ -29,6 +49,40 @@ class _ProfilePageState extends State<ProfilePage> {
           bool enabledDarkMode = isDark;
           return Stack(
             children: [
+              ListView(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(20),
+                          child: Image.asset("images/placeholder-profile.png",
+                              width: 160, height: 160),
+                        ),
+                        const SizedBox(height: 28),
+                        TextFormField(
+                          readOnly: true,
+                          initialValue: "John Doe",
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Full Name",
+                          ),
+                        ),
+                        const SizedBox(height: 28),
+                        TextFormField(
+                          readOnly: true,
+                          initialValue: "john.doe@email.com",
+                          decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: "Email Address",
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
               Positioned(
                 bottom: 20,
                 right: 20,
