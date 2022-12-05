32406a4cbec1f497ca22e10cf8b289b942d16dc9
Brendon <brendon.franco@vibryt.com.br>
2022-12-05 01:37:23 -0300

    feat: add the get request for games

--- lib/pages/home/all_games.dart
+++ lib/pages/home/all_games.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:meevun_2/pages/search/search.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 import 'add_new_game.dart';
 import 'package:http/http.dart' as http;
+
+import 'game_view.dart';
 
 class AllGamesPage extends StatefulWidget {
   const AllGamesPage({super.key});
@@ -14,34 +17,26 @@ class AllGamesPage extends StatefulWidget {
 
 class AllGamesPageState extends State<AllGamesPage> {
   List gamesDecoded = [];
-  List names = [];
-  List descriptions = [];
-  List images = [];
 
   Future<List> showAllGames() async {
-    try {
-      final response = await http.get(
-        Uri.parse('http://localhost:3000/api/v1/games'),
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded',
-          'Authorization':
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MzhiNGRhNWQwMDVlM2JhOGU3OTg1YjAiLCJlbWFpbCI6ImJyZW5kb24uZnJhbmNvbGl2ZWlyYUBnbWFpbC5jb20iLCJpYXQiOjE2NzAwNzg4MDAsImV4cCI6MTY3MDY4MzYwMH0.yPRTZKfSntGzfyerTpg4YzN9xXEjiS0xbaPHAfHFDDo',
-        },
-      );
+    await Future.delayed(const Duration(seconds: 1));
 
-      gamesDecoded = json.decode(response.body) as List;
-      names = gamesDecoded.map<String>((e) => e['name']).toList();
-      descriptions = gamesDecoded.map<String>((e) => e['description']).toList();
-      images = gamesDecoded.map<String>((e) => e['image']).toList();
-    } catch (e) {
-      // ignore: avoid_print
-    }
-    return [];
+    final response = await http.get(
+      Uri.parse('http://localhost:3000/api/v1/games'),
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization':
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2MzhiNGRhNWQwMDVlM2JhOGU3OTg1YjAiLCJlbWFpbCI6ImJyZW5kb24uZnJhbmNvbGl2ZWlyYUBnbWFpbC5jb20iLCJpYXQiOjE2NzAwNzg4MDAsImV4cCI6MTY3MDY4MzYwMH0.yPRTZKfSntGzfyerTpg4YzN9xXEjiS0xbaPHAfHFDDo',
+      },
+    );
+
+    gamesDecoded = json.decode(response.body) as List;
+
+    return gamesDecoded;
   }
 
   @override
   Widget build(BuildContext context) {
-    showAllGames();
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
@@ -98,25 +93,90 @@ class AllGamesPageState extends State<AllGamesPage> {
         ),
       ),
       body: FutureBuilder(
-        builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-          List<Widget> children = [];
+        future: showAllGames(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List allGames = snapshot.data!;
+            List names = allGames.map<String>((e) => e['name']).toList();
+            List images = allGames.map<String>((e) => e['image']).toList();
 
-          Timer(
-            const Duration(seconds: 1),
-            () {
-              for (var name in names) {
-                children.add(Text(name));
-              }
-            },
-          );
-
-          return ListView(
-            children: [
-              Column(
-                children: children,
-              ),
-            ],
-          );
+            return ListView(
+              children: [
+                ResponsiveGridRow(
+                  children: [
+                    for (int i = 0; i < allGames.length; i++)
+                      ResponsiveGridCol(
+                        lg: 3,
+                        md: 6,
+                        xs: 12,
+                        child: Card(
+                          elevation: 5,
+                          margin: const EdgeInsets.fromLTRB(15, 0, 15, 30),
+                          color: Theme.of(context).colorScheme.surfaceVariant,
+                          child: InkWell(
+                            splashColor:
+                                Theme.of(context).colorScheme.surfaceVariant,
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => const GameViewPage(),
+                                ),
+                              );
+                            },
+                            child: SizedBox(
+                              height: 300,
+                              child: Stack(
+                                children: [
+                                  ClipRRect(
+                                    borderRadius: const BorderRadius.only(
+                                      topLeft: Radius.circular(14),
+                                      topRight: Radius.circular(14),
+                                    ),
+                                    child: Image.network(
+                                      images[i],
+                                      height: 200,
+                                      width: double.infinity,
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 30,
+                                    left: 15,
+                                    child: Text(
+                                      names[i].toUpperCase(),
+                                      style: const TextStyle(
+                                        fontSize: 28,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    right: 10,
+                                    bottom: 110,
+                                    child: FloatingActionButton(
+                                      heroTag: null,
+                                      child: const Icon(Icons.star_outline),
+                                      onPressed: () {
+                                        debugPrint("Make favorite");
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ],
+            );
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
       ),
     );
